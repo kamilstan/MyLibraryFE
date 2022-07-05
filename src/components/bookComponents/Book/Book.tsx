111846fe9b24b28741b1f01a6ae5fa169546b342
kamilstangret@wp.pl <kamilstangret@wp.plgit config --global user.email kamilstangret@wp.pl>
2022-07-05 09:32:01 +0200

    Add User element and add delete buttons with deleteUser and deleteBook functions

--- src/components/bookComponents/Book/Book.tsx
+++ src/components/bookComponents/Book/Book.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { BookEntity } from "types";
 
 interface Props {
@@ -7,7 +7,8 @@ interface Props {
 
 export const Book =(props: Props)=> {
 
-    const [book, setBook] = useState<BookEntity | null>(null)
+    const [book, setBook] = useState<BookEntity | null>(null);
+
     useEffect(() => {
         (async () => {
             const res = await fetch(`http://localhost:3001/book/${props.id}`);
@@ -15,10 +16,18 @@ export const Book =(props: Props)=> {
             setBook(data);
 
         })()
-    })
-    if(book === null) {
-        return <p>Loading...</p>
+    }, [book])
+
+    const deleteBook = async () => {
+        await fetch(`http://localhost:3001/book/${props.id}`, {
+            method: "DELETE",
+        });
     }
+
+    if(book === null) {
+        return null
+    }
+
     return (
         <li  className="singleBook">
             <p><b>Title:</b> <span>{book.title}</span></p>
@@ -26,6 +35,7 @@ export const Book =(props: Props)=> {
             <p><b>Description:</b> <span>{book.description}</span></p>
             <p><b>Review:</b> <span>{book.review}</span></p>
             <button className="button">Take</button>
+            <button className="button" onClick={deleteBook}>Delete</button>
         </li>
     )
 }
